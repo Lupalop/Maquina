40f52c478f74f056d28e16e5fac1e5cff20c3b72
Francis Dominic Fajardo <fajardo@duck.com>
2018-11-04 19:05:20 +0800

    [SceneDictionary] Fix compilation errors

--- src/UI/SceneDictionary.cs
+++ src/UI/SceneDictionary.cs
@@ -8,19 +8,19 @@ namespace Maquina.UI
 {
     public class SceneDictionary<T> : Dictionary<T, SceneBase>
     {
-        public override void Add(T key, SceneBase scene)
+        public new void Add(T key, SceneBase scene)
         {
             // Load content when scene is added
             scene.LoadContent();
             base.Add(key, scene);
         }
-        public override bool Remove(T key)
+        public new bool Remove(T key)
         {
             // Unload content when scene is removed
             this[key].Unload();
             return base.Remove(key);
         }
-        public override void Clear()
+        public new void Clear()
         {
             // Unload content of every scene
             foreach (SceneBase scene in this.Values)
