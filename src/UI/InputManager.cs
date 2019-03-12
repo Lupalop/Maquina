399a7f97b273c60013e0483f6fab3a63f6ee76eb
Francis Dominic Fajardo <fajardo@duck.com>
2019-03-12 17:29:14 +0800

    [InputManager] Minor changes * Add methods- MouseDown and MouseUp * Reduce accessibility level of control properties

--- src/UI/InputManager.cs
+++ src/UI/InputManager.cs
@@ -15,7 +15,7 @@ namespace Maquina.UI
     {
         public InputManager(Game game)
         {
-            this._game = game;
+            this.game = game;
             game.Activated += delegate
             {
                 ShouldAcceptInput = true;
@@ -27,17 +27,17 @@ namespace Maquina.UI
             ShouldAcceptInput = true;
         }
 
-        private Game _game { get; set; }
+        private Game game;
         // Previous state
-        public KeyboardState PreviousKeyboardState { get; set; }
-        public GamePadState PreviousGamepadState { get; set; }
-        public MouseState PreviousMouseState { get; set; }
-        public TouchPanelState PreviousTouchState { get; set; }
+        public KeyboardState PreviousKeyboardState { get; private set; }
+        public GamePadState PreviousGamepadState { get; private set; }
+        public MouseState PreviousMouseState { get; private set; }
+        public TouchPanelState PreviousTouchState { get; private set; }
         // Current state
-        public KeyboardState KeyboardState { get; set; }
-        public GamePadState GamepadState { get; set; }
-        public MouseState MouseState { get; set; }
-        public TouchPanelState TouchState { get; set; }
+        public KeyboardState KeyboardState { get; private set; }
+        public GamePadState GamepadState { get; private set; }
+        public MouseState MouseState { get; private set; }
+        public TouchPanelState TouchState { get; private set; }
         // Determine if input should be accepted
         public bool ShouldAcceptInput { get; set; }
 
@@ -69,15 +69,58 @@ namespace Maquina.UI
             switch (mb)
             {
                 case MouseButton.Left:
-                    return MouseState.LeftButton == ButtonState.Pressed && PreviousMouseState.LeftButton == ButtonState.Released;
+                    return MouseState.LeftButton == ButtonState.Pressed &&
+                        PreviousMouseState.LeftButton == ButtonState.Released;
                 case MouseButton.Middle:
-                    return MouseState.MiddleButton == ButtonState.Pressed && PreviousMouseState.MiddleButton == ButtonState.Released;
+                    return MouseState.MiddleButton == ButtonState.Pressed &&
+                        PreviousMouseState.MiddleButton == ButtonState.Released;
                 case MouseButton.Right:
-                    return MouseState.RightButton == ButtonState.Pressed && PreviousMouseState.RightButton == ButtonState.Released;
+                    return MouseState.RightButton == ButtonState.Pressed &&
+                        PreviousMouseState.RightButton == ButtonState.Released;
                 case MouseButton.XButton1:
-                    return MouseState.XButton1 == ButtonState.Pressed && PreviousMouseState.XButton1 == ButtonState.Released;
+                    return MouseState.XButton1 == ButtonState.Pressed &&
+                        PreviousMouseState.XButton1 == ButtonState.Released;
                 case MouseButton.XButton2:
-                    return MouseState.XButton2 == ButtonState.Pressed && PreviousMouseState.XButton2 == ButtonState.Released;
+                    return MouseState.XButton2 == ButtonState.Pressed &&
+                        PreviousMouseState.XButton2 == ButtonState.Released;
+            }
+            return false;
+        }
+        public bool MouseDown(MouseButton mb)
+        {
+            if (!ShouldAcceptInput)
+                return false;
+            switch (mb)
+            {
+                case MouseButton.Left:
+                    return MouseState.LeftButton == ButtonState.Pressed;
+                case MouseButton.Middle:
+                    return MouseState.MiddleButton == ButtonState.Pressed;
+                case MouseButton.Right:
+                    return MouseState.RightButton == ButtonState.Pressed;
+                case MouseButton.XButton1:
+                    return MouseState.XButton1 == ButtonState.Pressed;
+                case MouseButton.XButton2:
+                    return MouseState.XButton2 == ButtonState.Pressed;
+            }
+            return false;
+        }
+        public bool MouseUp(MouseButton mb)
+        {
+            if (!ShouldAcceptInput)
+                return false;
+            switch (mb)
+            {
+                case MouseButton.Left:
+                    return MouseState.LeftButton == ButtonState.Released;
+                case MouseButton.Middle:
+                    return MouseState.MiddleButton == ButtonState.Released;
+                case MouseButton.Right:
+                    return MouseState.RightButton == ButtonState.Released;
+                case MouseButton.XButton1:
+                    return MouseState.XButton1 == ButtonState.Released;
+                case MouseButton.XButton2:
+                    return MouseState.XButton2 == ButtonState.Released;
             }
             return false;
         }
@@ -93,7 +136,7 @@ namespace Maquina.UI
             GamepadState = GamePad.GetState(PlayerIndex.One);
             KeyboardState = Keyboard.GetState();
             MouseState = Mouse.GetState();
-            TouchState = TouchPanel.GetState(_game.Window);
+            TouchState = TouchPanel.GetState(game.Window);
         }
     }
 }
