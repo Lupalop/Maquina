a44b681ff61a1594bd64744e17a54caeed656df9
Francis Dominic Fajardo <fajardo@duck.com>
2019-07-11 17:36:13 +0800

    Stub out progress bar element

---
+++ src/Elements/BarSprite.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Maquina.Elements;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Maquina.Elements
+{
+    public class BarSprite : Sprite
+    {
+        // TODO: Rewrite bar sprite
+    }
+}
