4343a04d5c556be952289f74a30bc985ce3be413
Francis Dominic Fajardo <fajardo@duck.com>
2019-10-14 19:36:29 +0800

    Move Scene auto position layout code to GuiElement - Scenes no longer have to enumerate the dictionary and check for the value of auto position. - GuiElement now checks for the auto position property and updates accordingly.

--- src/Elements/GuiElement.cs
+++ src/Elements/GuiElement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -37,10 +38,107 @@ namespace Maquina.Elements
             }
         }
 
-        public bool AutoPosition { get; set; }
         public override string Id
         {
             get { return "GENERIC_GUI"; }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                AutoPosition = false;
+            }
+            base.Dispose(disposing);
+        }
+
+        // Element Auto Position implementation
+        private bool autoPosition;
+        public bool AutoPosition
+        {
+            get { return autoPosition; }
+            set
+            {
+                autoPosition = value;
+                // Register to events necessary to update in time
+                if (value)
+                {
+                    Global.Display.ResolutionChanged += Display_ResolutionChanged;
+                    Global.ScaleChanged += Global_ScaleChanged;
+                    ElementChanged += GuiElement_ElementChanged;
+                }
+                // Only unregister from these events if auto position was previously true
+                if (autoPosition && !value)
+                {
+                    Global.Display.ResolutionChanged -= Display_ResolutionChanged;
+                    Global.ScaleChanged -= Global_ScaleChanged;
+                    ElementChanged -= GuiElement_ElementChanged;
+                }
+            }
+        }
+
+        public void UpdateAutoPositionedLayout()
+        {
+            // Ignore elements that are not requesting auto position
+            if (!AutoPosition)
+            {
+                return;
+            }
+
+            int modifiedElementX = Location.X;
+            int modifiedElementY = Location.Y;
+
+            switch (HorizontalAlignment)
+            {
+                case HorizontalAlignment.Left:
+                    modifiedElementX = Global.Display.WindowBounds.Left;
+                    break;
+                case HorizontalAlignment.Center:
+                    if (ActualBounds.Width != 0)
+                        modifiedElementX = Global.Display.WindowBounds.Center.X - (ActualBounds.Width / 2);
+                    break;
+                case HorizontalAlignment.Right:
+                    modifiedElementX = Global.Display.WindowBounds.Right - ActualBounds.Width;
+                    break;
+                case HorizontalAlignment.Stretch:
+                    break;
+            }
+
+            switch (VerticalAlignment)
+            {
+                case VerticalAlignment.Top:
+                    modifiedElementY = Global.Display.WindowBounds.Top;
+                    break;
+                case VerticalAlignment.Center:
+                    if (ActualBounds.Height != 0)
+                        modifiedElementY = Global.Display.WindowBounds.Center.Y - (ActualBounds.Height / 2);
+                    break;
+                case VerticalAlignment.Bottom:
+                    modifiedElementY = Global.Display.WindowBounds.Bottom - ActualBounds.Height;
+                    break;
+                case VerticalAlignment.Stretch:
+                    break;
+            }
+
+            Location = new Point(modifiedElementX, modifiedElementY);
+        }
+
+        private void Display_ResolutionChanged(Rectangle obj)
+        {
+            UpdateAutoPositionedLayout();
+        }
+
+        private void Global_ScaleChanged(object sender, float e)
+        {
+            UpdateAutoPositionedLayout();
+        }
+
+        private void GuiElement_ElementChanged(object sender, ElementChangedEventArgs e)
+        {
+            if (e.Property == ElementChangedProperty.Location)
+                return;
+
+            UpdateAutoPositionedLayout();
         }
     }
 }
--- src/UI/Scene.cs
+++ src/UI/Scene.cs
@@ -30,32 +30,13 @@ namespace Maquina.UI
             IsFirstUpdateDone = false;
         }
 
-        private void DisplayManager_ResolutionChanged(Rectangle obj)
-        {
-            UpdateLayout(Elements);
-        }
-
-        private void Global_ScaleChanged(object sender, float e)
-        {
-            UpdateLayout(Elements);
-        }
-
         protected SceneManager SceneManager { get; private set; }
         protected InputManager InputManager { get; private set; }
         protected Game Game { get; private set; }
         protected SpriteBatch SpriteBatch { get; private set; }
         protected Dictionary<string, SpriteFont> Fonts { get; private set; }
 
-        private IDictionary<string, BaseElement> elements;
-        public IDictionary<string, BaseElement> Elements
-        {
-            get { return elements; }
-            set
-            {
-                elements = value;
-                UpdateLayout(Elements);
-            }
-        }
+        public IDictionary<string, BaseElement> Elements { get; set; }
         public string SceneName { get; private set; }
         private bool IsFirstUpdateDone;
 
@@ -79,11 +60,6 @@ namespace Maquina.UI
             {
                 LoadContentFinished(this, EventArgs.Empty);
             }
-            // Listen to resolution change events in order to update layout when needed
-            Global.Display.ResolutionChanged += DisplayManager_ResolutionChanged;
-            Global.ScaleChanged += Global_ScaleChanged;
-            // Update layout after all elements were loaded
-            UpdateLayout(Elements);
         }
 
         public virtual void Draw(GameTime gameTime) { }
@@ -99,8 +75,6 @@ namespace Maquina.UI
         public virtual void Unload()
         {
             DisposeElements(Elements);
-            Global.Display.ResolutionChanged -= DisplayManager_ResolutionChanged;
-            Global.ScaleChanged -= Global_ScaleChanged;
 #if LOG_ENABLED
             LogManager.Info(0, string.Format("Unloaded content from scene: {0}", SceneName));
 #endif
@@ -157,77 +131,6 @@ namespace Maquina.UI
                 BaseElement element = elements.ElementAt(i);
                 element.Update(gameTime);
             }
-        }
-
-        public virtual void UpdateLayout(IDictionary<string, BaseElement> elements)
-        {
-            UpdateLayout(elements.Values);
-        }
-        public virtual void UpdateLayout(IEnumerable<BaseElement> elements)
-        {
-            for (int i = 0; i < elements.Count(); i++)
-            {
-                BaseElement element = elements.ElementAt(i);
-
-                // Recurse if element is a container
-                if (element is IContainerElement)
-                {
-                    IContainerElement container = (IContainerElement)element;
-                    UpdateLayout(container.Children);
-                }
-
-                // Ignore non-UI elements
-                if (element is GuiElement)
-                {
-                    GuiElement modifiedElement = (GuiElement)element;
-                    
-                    // Ignore elements that are not requesting auto position
-                    if (!modifiedElement.AutoPosition)
-                    {
-                        return;
-                    }
-
-                    int modifiedElementX = modifiedElement.Location.X;
-                    int modifiedElementY = modifiedElement.Location.Y;
-
-                    switch (modifiedElement.HorizontalAlignment)
-                    {
-                        case HorizontalAlignment.Left:
-                            modifiedElementX = WindowBounds.Left;
-                            break;
-                        case HorizontalAlignment.Center:
-                            if (element.ActualBounds.Width != 0)
-                                modifiedElementX = WindowBounds.Center.X - (element.ActualBounds.Width / 2);
-                            break;
-                        case HorizontalAlignment.Right:
-                            modifiedElementX = WindowBounds.Right - element.ActualBounds.Width;
-                            break;
-                        case HorizontalAlignment.Stretch:
-                            break;
-                    }
-
-                    switch (modifiedElement.VerticalAlignment)
-                    {
-                        case VerticalAlignment.Top:
-                            modifiedElementY = WindowBounds.Top;
-                            break;
-                        case VerticalAlignment.Center:
-                            if (element.ActualBounds.Height != 0)
-                                modifiedElementY = WindowBounds.Center.Y - (element.ActualBounds.Height / 2);
-                            break;
-                        case VerticalAlignment.Bottom:
-                            modifiedElementY = WindowBounds.Bottom - element.ActualBounds.Height;
-                            break;
-                        case VerticalAlignment.Stretch:
-                            break;
-                    }
-
-                    modifiedElement.Location = new Point(modifiedElementX, modifiedElementY);
-                }
-            }
-#if LOG_ENABLED
-            LogManager.Info(0, string.Format("Updated scene layout: {0}", SceneName));
-#endif
         }
     }
 }
