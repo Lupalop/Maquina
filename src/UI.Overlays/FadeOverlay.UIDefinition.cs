60a01e7ea104ef7e3edec022f3dae35280c41990
Francis Dominic Fajardo <fajardo@duck.com>
2019-10-26 10:41:37 +0800

    Update fade overlay to use animations and separate UI definition code

---
+++ src/UI.Overlays/FadeOverlay.UIDefinition.cs
@@ -0,0 +1,46 @@
+﻿using Maquina.Elements;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maquina.UI
+{
+    public partial class FadeOverlay
+    {
+        Image FadeImage;
+
+        private void InitializeComponent()
+        {
+            FadeImage = new Image("Background")
+            {
+                IgnoreGlobalScale = true,
+                Graphic = FadeBackground,
+            };
+            FadeImage.Background.DestinationRectangle = WindowBounds;
+
+            Global.Display.ResolutionChanged += (sender, e) =>
+            {
+                FadeImage.Background.DestinationRectangle = ((DisplayManager)sender).WindowBounds;
+            };
+
+            FadeInAnimation = new FadeInAnimation(FadeImage.Background, FadeSpeed);
+            FadeInAnimation.AnimationFinished += (sender, e) =>
+            {
+                FadeOutAnimation.Start();
+            };
+
+            FadeOutAnimation = new FadeOutAnimation(FadeImage.Background, FadeSpeed);
+            FadeOutAnimation.AnimationFinished += (sender, e) =>
+            {
+                SceneManager.Overlays.Remove(OverlayKey);
+            };
+
+            FadeInAnimation.Start();
+
+            Elements.Add(FadeImage.Name, FadeImage);
+        }
+    }
+}
--- src/UI.Overlays/FadeOverlay.cs
+++ src/UI.Overlays/FadeOverlay.cs
@@ -10,39 +10,29 @@ using Maquina.Elements;
 
 namespace Maquina.UI
 {
-    public class FadeOverlay : Overlay, IDisposable
+    public partial class FadeOverlay : Overlay, IDisposable
     {
-        public FadeOverlay(string overlayKey)
-            : base("Fade Overlay")
+        public FadeOverlay(string overlayKey, Color fadeColor) : base("Fade Overlay")
         {
-            this.OverlayKey = overlayKey;
-            this.FadeColor = Color.Black;
-            this.FadeSpeed = 0.1f;
+            OverlayKey = overlayKey;
+            FadeColor = fadeColor;
+            FadeSpeed = 0.1f;
         }
+        public FadeOverlay(string overlayKey) : this(overlayKey, Color.Black) { }
 
-        public FadeOverlay(string overlayKey, Color fadeColor)
-            : base("Fade Overlay")
-        {
-            this.OverlayKey = overlayKey;
-            this.FadeColor = fadeColor;
-            this.FadeSpeed = 0.1f;
-        }
-
-        private float Opacity = 1f;
         private string OverlayKey;
         private Texture2D FadeBackground;
+        public FadeInAnimation FadeInAnimation { get; private set; }
+        public FadeOutAnimation FadeOutAnimation { get; private set; }
         public float FadeSpeed { get; set; }
 
-        private Color _fadeColor;
+        private Color fadeColor;
         public Color FadeColor
         {
-            get
+            get { return fadeColor; }
+            private set
             {
-                return _fadeColor;
-            }
-            set
-            {
-                _fadeColor = value;
+                fadeColor = value;
                 FadeBackground = new Texture2D(Game.GraphicsDevice, 1, 1);
                 FadeBackground.SetData(new Color[] { value });
             }
@@ -50,18 +40,7 @@ namespace Maquina.UI
 
         public override void LoadContent()
         {
-            Image Background = new Image("Background")
-            {
-                IgnoreGlobalScale = true,
-            };
-            Background.ElementUpdated += (sender, e) =>
-            {
-                Background.Graphic = FadeBackground;
-                Background.Background.DestinationRectangle = Game.GraphicsDevice.Viewport.Bounds;
-                Background.Background.Tint = FadeColor * Opacity;
-            };
-            Elements.Add(Background.Name, Background);
-
+            InitializeComponent();
             base.LoadContent();
         }
 
@@ -75,16 +54,8 @@ namespace Maquina.UI
 
         public override void Update(GameTime gameTime)
         {
-            Opacity -= FadeSpeed;
-
             base.Update(gameTime);
             base.UpdateElements(gameTime, Elements);
-
-            // Remove overlay when opacity below 0
-            if (Opacity <= 0f)
-            {
-                SceneManager.Overlays.Remove(OverlayKey);
-            }
         }
 
         public void Dispose()
