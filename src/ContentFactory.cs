e7fb4992b1300ce38fa5c0136b24dc639a1ba763
Francis Dominic Fajardo <fajardo@duck.com>
2020-04-08 16:11:57 +0800

    Warn in logger when requesting a missing resource

--- src/ContentFactory.cs
+++ src/ContentFactory.cs
@@ -26,6 +26,9 @@ namespace Maquina
                 targetContainer.TryGetValue(resourceName, out targetResource);
                 return targetResource;
             }
+#if LOG_ENABLED
+            LogManager.Warn(0, $"Missing resource: {containerName}, {resourceName}");
+#endif
             return null;
         }
 
@@ -38,6 +41,9 @@ namespace Maquina
                     return item.Value[resourceName];
                 }
             }
+#if LOG_ENABLED
+            LogManager.Warn(0, $"Missing resource: {resourceName}");
+#endif
             return null;
         }
     }
