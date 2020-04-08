32ca917486c134e64426ee6c9e0e399101012a6a
Francis Dominic Fajardo <fajardo@duck.com>
2020-04-08 16:15:45 +0800

    Remove MergeWith dictionary type extension

--- src/TypeExtensions.cs
+++ src/TypeExtensions.cs
@@ -9,22 +9,5 @@ namespace Maquina
 {
     public static class TypeExtensions
     {
-        public static Dictionary<TKey, TValue> MergeWith<TKey, TValue>(
-            this IDictionary<TKey, TValue> dictionaryA,
-            IDictionary<TKey, TValue> dictionaryB,
-            bool useFirstOnDuplicate = false)
-        {
-            // Use first occurence when there is a duplicate
-            if (useFirstOnDuplicate)
-            {
-                return dictionaryA.Concat(dictionaryB)
-                    .GroupBy(e => e.Key)
-                    .ToDictionary(g => g.Key, g => g.First().Value);
-            }
-            // Use the last (the one from dictionary B) when there is a duplicate
-            return dictionaryA.Concat(dictionaryB)
-                    .GroupBy(e => e.Key)
-                    .ToDictionary(g => g.Key, g => g.Last().Value);
-        }
     }
 }
