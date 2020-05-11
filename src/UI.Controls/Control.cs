6e7a52297dbff147bfd28dbfa757907b3e0eb777
Francis Dominic Fajardo <fajardo@duck.com>
2020-05-11 12:53:41 +0800

    Move Control class to Maquina.UI namespace

--- src/UI.Controls/Control.cs
+++ src/UI.Controls/Control.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Maquina.Elements;
+using Microsoft.Xna.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Maquina.Elements
+namespace Maquina.UI
 {
     public abstract class Control : BaseElement
     {
