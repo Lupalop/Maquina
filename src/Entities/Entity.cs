5dd49c46c8d5e6399d54c171d531f0b8ef97bc73
Francis Dominic Fajardo <fajardo@duck.com>
2020-07-01 21:48:01 +0800

    Include documentation for the Entity class

--- src/Entities/Entity.cs
+++ src/Entities/Entity.cs
@@ -15,6 +15,11 @@ namespace Maquina.Entities
         private float _scale;
         private bool _ignoreGlobalScale;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Entity"/> class
+        /// with a specified name.
+        /// </summary>
+        /// <param name="name">A name that describes the entity.</param>
         protected Entity(string name)
         {
             Id = "GENERIC_BASE";
@@ -22,9 +27,19 @@ namespace Maquina.Entities
             Scale = 1;
         }
 
+        /// <summary>
+        /// Gets or sets the name of the entity.
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets the unique identifier of the entity.
+        /// </summary>
         public string Id { get; protected set; }
 
+        /// <summary>
+        /// Gets or sets the drawing location of the entity on the screen.
+        /// </summary>
         public virtual Point Location
         {
             get { return _location; }
@@ -39,6 +54,9 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets or sets the dimensions of the entity.
+        /// </summary>
         public virtual Point Size
         {
             get { return _size; }
@@ -53,6 +71,9 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets or sets the scaling of the entity.
+        /// </summary>
         public float Scale
         {
             get { return _scale; }
@@ -63,12 +84,18 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets or sets the behavior of the entity when dealing with the global scale.
+        /// </summary>
         public bool IgnoreGlobalScale
         {
             get { return _ignoreGlobalScale; }
             set { _ignoreGlobalScale = value; }
         }
 
+        /// <summary>
+        /// Gets or sets the drawing bounds of the entity.
+        /// </summary>
         public virtual Rectangle Bounds
         {
             get { return new Rectangle(Location, Size); }
@@ -80,6 +107,9 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets the factor to multiply the size of the entity, taking into account the global scale.
+        /// </summary>
         public float ActualScale
         {
             get
@@ -92,6 +122,9 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets the dimensions of the entity, taking into account the global scale.
+        /// </summary>
         public Point ActualSize
         {
             get
@@ -102,11 +135,17 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Gets the drawing bounds of the entity, taking into account the global scale.
+        /// </summary>
         public Rectangle ActualBounds
         {
             get { return new Rectangle(Location, ActualSize); }
         }
 
+        /// <summary>
+        /// Occurs when a property of the entity has been modified.
+        /// </summary>
         public event EventHandler<EntityChangedEventArgs> Changed;
 
         protected virtual void OnEntityChanged(EntityChangedEventArgs e)
@@ -117,10 +156,16 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// This is called when the entity should update itself.
+        /// </summary>
         public virtual void Update()
         {
         }
 
+        /// <summary>
+        /// This is called when the entity should draw itself.
+        /// </summary>
         public virtual void Draw()
         {
         }
@@ -133,6 +178,9 @@ namespace Maquina.Entities
             }
         }
 
+        /// <summary>
+        /// Releases the resources used by this <see cref="Entity"/>.
+        /// </summary>
         public void Dispose()
         {
             Dispose(true);
