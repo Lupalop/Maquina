c2541643783e22af5cecbbba565e7e25d4e38909
Francis Dominic Fajardo <fajardo@duck.com>
2020-07-02 15:17:22 +0800

    Remove IEntity interface

--- src/Entities/Entity.cs
+++ src/Entities/Entity.cs
@@ -8,7 +8,7 @@ using System.Collections.ObjectModel;
 
 namespace Maquina.Entities
 {
-    public abstract class Entity : IEntity, IDisposable
+    public abstract class Entity : IDisposable
     {
         private Point _location;
         private Point _size;
