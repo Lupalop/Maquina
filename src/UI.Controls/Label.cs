75df49bc9db229a5fb9d2c00f5fb474a9d2fc2d2
Francis Dominic Fajardo <fajardo@duck.com>
2020-07-02 15:18:17 +0800

    Remove TextDrawController and use the default DrawController for Label

--- src/UI.Controls/Label.cs
+++ src/UI.Controls/Label.cs
@@ -12,24 +12,58 @@ namespace Maquina.UI
 {
     public class Label : Control
     {
+        private SpriteFont _font;
+        private string _text;
+
         public Label(string name) : base (name)
         {
             Id = "UI_LABEL";
-            DrawController.Text = "";
+            DrawController = new DrawController();
+            Font = (SpriteFont)ContentFactory.TryGetResource("default");
+            Text = "";
         }
 
-        public TextDrawController DrawController { get; set; }
+        public DrawController DrawController { get; set; }
+
+        public SpriteFont Font
+        {
+            get { return _font; }
+            set
+            {
+                _font = value;
+            }
+        }
+
+        public string Text
+        {
+            get { return _text; }
+            set
+            {
+#if MGE_LOCALE
+                _text = Application.Locale.TryGetString(value);
+#else
+                text = value;
+#endif
+            }
+        }
 
         public override Point Size
         {
-            get { return DrawController.Size; }
+            get
+            {
+                if (Font != null && Text.Trim() != null)
+                {
+                    return Font.MeasureString(Text).ToPoint();
+                }
+                return Point.Zero;
+            }
         }
 
         public override void Draw(SpriteBatch spriteBatch)
         {
             spriteBatch.DrawString(
-                DrawController.Font,
-                DrawController.Text,
+                Font,
+                Text,
                 Location.ToVector2(),
                 DrawController.Tint * DrawController.Opacity,
                 DrawController.Rotation,
@@ -37,6 +71,10 @@ namespace Maquina.UI
                 ActualScale,
                 DrawController.SpriteEffects,
                 DrawController.LayerDepth);
+        }
+
+        public override void Update()
+        {
         }
     }
 }
