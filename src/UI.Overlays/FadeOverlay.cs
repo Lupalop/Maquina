7ac6814d7a04a1d6bb16db7dd480528b996340fc
Francis Dominic Fajardo <fajardo@duck.com>
2020-07-09 18:05:49 +0800

    Merge fade overlay source files

--- src/UI.Overlays/FadeOverlay.cs
+++ src/UI.Overlays/FadeOverlay.cs
@@ -12,31 +12,64 @@ namespace Maquina.UI
 {
     public partial class FadeOverlay : Overlay, IDisposable
     {
+        private Image _fadeImage;
+        private string _overlayKey;
+        private Texture2D _fadeBackground;
+        private Color _fadeColor;
+
         public FadeOverlay(string overlayKey, Color fadeColor) : base("Fade Overlay")
         {
-            OverlayKey = overlayKey;
+            _overlayKey = overlayKey;
             FadeColor = fadeColor;
             FadeSpeed = 0.1f;
 
             InitializeComponent();
         }
+
+        private void InitializeComponent()
+        {
+            _fadeImage = new Image("Background");
+            _fadeImage.IgnoreDisplayScale = true;
+            _fadeImage.Sprite = _fadeBackground;
+            _fadeImage.Bounds = Bounds;
+
+            Application.Display.ResolutionChanged += (sender, e) =>
+            {
+                _fadeImage.Bounds = ((DisplayManager)sender).WindowBounds;
+            };
+
+            FadeInAnimation = new FadeInAnimation(_fadeImage, FadeSpeed);
+            FadeInAnimation.AnimationFinished += (sender, e) =>
+            {
+                FadeOutAnimation.Start();
+            };
+
+            FadeOutAnimation = new FadeOutAnimation(_fadeImage, FadeSpeed);
+            FadeOutAnimation.AnimationFinished += (sender, e) =>
+            {
+                Application.Scenes.Overlays.Remove(this);
+            };
+
+            FadeInAnimation.Start();
+
+            Entities.Add(_fadeImage);
+        }
+
+        public FadeInAnimation FadeInAnimation { get; protected set; }
+        public FadeOutAnimation FadeOutAnimation { get; protected set; }
+
         public FadeOverlay(string overlayKey) : this(overlayKey, Color.Black) { }
 
-        private string OverlayKey;
-        private Texture2D FadeBackground;
-        public FadeInAnimation FadeInAnimation { get; private set; }
-        public FadeOutAnimation FadeOutAnimation { get; private set; }
         public float FadeSpeed { get; set; }
 
-        private Color fadeColor;
         public Color FadeColor
         {
-            get { return fadeColor; }
+            get { return _fadeColor; }
             private set
             {
-                fadeColor = value;
-                FadeBackground = new Texture2D(Game.GraphicsDevice, 1, 1);
-                FadeBackground.SetData(new Color[] { value });
+                _fadeColor = value;
+                _fadeBackground = new Texture2D(Game.GraphicsDevice, 1, 1);
+                _fadeBackground.SetData(new Color[] { value });
             }
         }
 
@@ -56,7 +89,7 @@ namespace Maquina.UI
         {
             if (disposing)
             {
-                FadeBackground.Dispose();
+                _fadeBackground.Dispose();
             }
             base.Dispose(disposing);
         }
