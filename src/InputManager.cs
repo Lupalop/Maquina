87277555a761232946a9fdee50d44b346bffc155
Francis Dominic Fajardo <fajardo@duck.com>
2020-11-28 15:36:54 +0800

    [InputManager] Invert condition checks for Enabled property

--- src/InputManager.cs
+++ src/InputManager.cs
@@ -42,83 +42,92 @@ namespace Maquina
         // Keyboard
         public bool KeyPressed(Keys key)
         {
-            if (!Enabled)
-                return false;
-            return KeyboardState.IsKeyDown(key) && PreviousKeyboardState.IsKeyUp(key);
+            if (Enabled)
+            {
+                return KeyboardState.IsKeyDown(key) && PreviousKeyboardState.IsKeyUp(key);
+            }
+            return false;
         }
         public bool KeyDown(Keys key)
         {
-            if (!Enabled)
-                return false;
-            return KeyboardState.IsKeyDown(key);
+            if (Enabled)
+            {
+                return KeyboardState.IsKeyDown(key);
+            }
+            return false;
         }
         public bool KeyUp(Keys key)
         {
-            if (!Enabled)
-                return false;
-            return KeyboardState.IsKeyUp(key);
+            if (Enabled)
+            {
+                return KeyboardState.IsKeyUp(key);
+            }
+            return false;
         }
 
         // Mouse
         public bool MousePressed(MouseButton mb)
         {
-            if (!Enabled)
-                return false;
-            switch (mb)
+            if (Enabled)
             {
-                case MouseButton.Left:
-                    return MouseState.LeftButton == ButtonState.Pressed &&
-                        PreviousMouseState.LeftButton == ButtonState.Released;
-                case MouseButton.Middle:
-                    return MouseState.MiddleButton == ButtonState.Pressed &&
-                        PreviousMouseState.MiddleButton == ButtonState.Released;
-                case MouseButton.Right:
-                    return MouseState.RightButton == ButtonState.Pressed &&
-                        PreviousMouseState.RightButton == ButtonState.Released;
-                case MouseButton.XButton1:
-                    return MouseState.XButton1 == ButtonState.Pressed &&
-                        PreviousMouseState.XButton1 == ButtonState.Released;
-                case MouseButton.XButton2:
-                    return MouseState.XButton2 == ButtonState.Pressed &&
-                        PreviousMouseState.XButton2 == ButtonState.Released;
+                switch (mb)
+                {
+                    case MouseButton.Left:
+                        return MouseState.LeftButton == ButtonState.Pressed &&
+                            PreviousMouseState.LeftButton == ButtonState.Released;
+                    case MouseButton.Middle:
+                        return MouseState.MiddleButton == ButtonState.Pressed &&
+                            PreviousMouseState.MiddleButton == ButtonState.Released;
+                    case MouseButton.Right:
+                        return MouseState.RightButton == ButtonState.Pressed &&
+                            PreviousMouseState.RightButton == ButtonState.Released;
+                    case MouseButton.XButton1:
+                        return MouseState.XButton1 == ButtonState.Pressed &&
+                            PreviousMouseState.XButton1 == ButtonState.Released;
+                    case MouseButton.XButton2:
+                        return MouseState.XButton2 == ButtonState.Pressed &&
+                            PreviousMouseState.XButton2 == ButtonState.Released;
+                }
             }
             return false;
         }
         public bool MouseDown(MouseButton mb)
         {
-            if (!Enabled)
-                return false;
-            switch (mb)
+            if (Enabled)
             {
-                case MouseButton.Left:
-                    return MouseState.LeftButton == ButtonState.Pressed;
-                case MouseButton.Middle:
-                    return MouseState.MiddleButton == ButtonState.Pressed;
-                case MouseButton.Right:
-                    return MouseState.RightButton == ButtonState.Pressed;
-                case MouseButton.XButton1:
-                    return MouseState.XButton1 == ButtonState.Pressed;
-                case MouseButton.XButton2:
-                    return MouseState.XButton2 == ButtonState.Pressed;
+                switch (mb)
+                {
+                    case MouseButton.Left:
+                        return MouseState.LeftButton == ButtonState.Pressed;
+                    case MouseButton.Middle:
+                        return MouseState.MiddleButton == ButtonState.Pressed;
+                    case MouseButton.Right:
+                        return MouseState.RightButton == ButtonState.Pressed;
+                    case MouseButton.XButton1:
+                        return MouseState.XButton1 == ButtonState.Pressed;
+                    case MouseButton.XButton2:
+                        return MouseState.XButton2 == ButtonState.Pressed;
+                }
             }
             return false;
         }
         public bool MouseUp(MouseButton mb)
         {
-            if (!Enabled)
-                return false;
-            switch (mb)
+            if (Enabled)
             {
-                case MouseButton.Left:
-                    return MouseState.LeftButton == ButtonState.Released;
-                case MouseButton.Middle:
-                    return MouseState.MiddleButton == ButtonState.Released;
-                case MouseButton.Right:
-                    return MouseState.RightButton == ButtonState.Released;
-                case MouseButton.XButton1:
-                    return MouseState.XButton1 == ButtonState.Released;
-                case MouseButton.XButton2:
-                    return MouseState.XButton2 == ButtonState.Released;
+                switch (mb)
+                {
+                    case MouseButton.Left:
+                        return MouseState.LeftButton == ButtonState.Released;
+                    case MouseButton.Middle:
+                        return MouseState.MiddleButton == ButtonState.Released;
+                    case MouseButton.Right:
+                        return MouseState.RightButton == ButtonState.Released;
+                    case MouseButton.XButton1:
+                        return MouseState.XButton1 == ButtonState.Released;
+                    case MouseButton.XButton2:
+                        return MouseState.XButton2 == ButtonState.Released;
+                }
             }
             return false;
         }
